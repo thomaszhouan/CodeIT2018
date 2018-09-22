4b317f6511f64b99fe9ff6bcb0bfa2bac1681dc5
zzy <zzhangcm@connect.ust.hk>
2018-09-22 11:50:18 +0800

    tally expenses

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.primesum
-import codeitsuisse.routes.tally expenses
+import codeitsuisse.routes.tally_expenses
 
