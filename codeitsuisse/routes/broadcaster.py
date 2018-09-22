75bfaac9bf30a699db163cfd56d98e0a44c92d54
agong980814 <agong980814@gmail.com>
2018-09-22 23:58:23 +0800

    fastest path

--- codeitsuisse/routes/broadcaster.py
+++ codeitsuisse/routes/broadcaster.py
@@ -12,6 +12,7 @@ from flask import request, jsonify
 from codeitsuisse import app
 
 import queue
+import heapq
 
 logger = logging.getLogger(__name__)
 
@@ -53,4 +54,69 @@ def most_connected_node():
     return jsonify(result)
 
 
+def solve_shortest_path(data):
+    node2id = {}
+    nodes = []
 
+    def get_id(node):
+        if node in node2id:
+            return node2id[node]
+        n = len(node2id)
+        node2id[node] = n
+        nodes.append(node)
+        return n
+
+    edge = []
+    # print(data['data'])
+    for foo in data['data']:
+        foo0, w = foo.split(',')
+        u, v = foo0.split('->')
+        u = get_id(u)
+        v = get_id(v)
+        w = int(w)
+        edge.append((u, v, w))
+    # print(edge)
+    # print(nodes)
+    # print(node2id)
+
+    n = len(node2id)
+    g = []
+    for i in range(n):
+        g.append([])
+    for i, (u, _, _) in enumerate(edge):
+        g[u].append(i)
+    # print(g)
+
+    p = [-1] * n
+    d = [-1] * n
+    vis = [False] * n
+    s = get_id(data['sender'])
+    t = get_id(data['recipient'])
+    q = [(0, s)]
+    heapq.heapify(q)
+    d[s] = 0
+    while len(q) > 0:
+        foo = heapq.heappop(q)
+        u = foo[1]
+        if vis[u]:
+            continue
+        vis[u] = True
+        if u == t:
+            path = []
+            while u != -1:
+                path.append(nodes[u])
+                u = p[u]
+            return reversed(path)
+        for i in g[u]:
+            _, v, w = edge[i]
+            if d[v]<0 or d[v]>w+d[u]:
+                d[v] = w+d[u]
+                p[v] = u
+                heapq.heappush(q, (d[v], v))
+    assert False
+
+
+@app.route('/broadcaster/fastest-path', methods=['POST'])
+def fastest_path():
+    data = request.get_json()
+    return jsonify({'result': solve_shortest_path(data)})
