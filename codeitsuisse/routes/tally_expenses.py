a0b146fb4533c5ce943434224b80cdffeed3b0c1
zzy <zzhangcm@connect.ust.hk>
2018-09-22 18:10:30 +0800

    tally_expenses

--- codeitsuisse/routes/tally_expenses.py
+++ codeitsuisse/routes/tally_expenses.py
@@ -50,68 +50,70 @@ def tally_expense():
     transaction = np.zeros(num_person)
     for i in range(num_person):
         transaction[i] = money[persons[i]]
-    for i in range(num_person):
-        while transaction[i] != 0:
-            tran = transaction[i]
-            if transaction[i] > 0:               
-                minindex = np.argmin(transaction)
-                minm = transaction[minindex]
-                if tran + minm > 0:
-                    transaction[i] = tran + minm
-                    transaction[minindex] = 0
-                    temp = {}
-                    temp["from"] = persons[minindex]
-                    temp["to"] = persons[i]
-                    temp["amount"] = - minm
-                    result.append(temp)
-                else:
-                    transaction[i] = 0
-                    transaction[minindex] = tran + minm
-                    temp = {}
-                    temp["from"] = persons[minindex]
-                    temp["to"] = persons[i]
-                    temp["amount"] = tran
-                    result.append(temp)
-            else:
-                maxindex = np.argmax(transaction)
-                maxm = transaction[maxindex]
-                if tran + maxm > 0:
-                    transaction[i] = 0
-                    transaction[maxindex] = tran + maxm
-                    temp = {}
-                    temp["from"] = persons[i]
-                    temp["to"] = persons[maxindex]
-                    temp["amount"] = - tran
-                    result.append(temp)
-                else:
-                    transaction[maxindex] = 0
-                    transaction[i] = tran + maxm
-                    temp = {}
-                    temp["from"] = persons[i]
-                    temp["to"] = persons[maxindex]
-                    temp["amount"] = maxm
-                    result.append(temp)
-#    while max(transaction) > 0:
-#        maxindex = np.argmax(transaction)
-#        minindex = np.argmin(transaction)
-#        maxm = transaction[maxindex]
-#        minm = transaction[minindex]
-#        if maxm + minm > 0:
-#            transaction[maxindex] = maxm + minm
-#            transaction[minindex] = 0
-#            temp = {}
-#            temp["from"] = persons[minindex]
-#            temp["to"] = persons[maxindex]
-#            temp["amount"] = - minm
-#            result.append(temp)
-#        else:
-#            transaction[maxindex] = 0
-#            transaction[minindex] = maxm + minm
-#            temp = {}
-#            temp["from"] = persons[minindex]
-#            temp["to"] = persons[maxindex]
-#            temp["amount"] = maxm
-#            result.append(temp)
+#    for i in range(num_person):
+#        while transaction[i] != 0:
+#            tran = transaction[i]
+#            if transaction[i] > 0:               
+#                minindex = np.argmin(transaction)
+#                minm = transaction[minindex]
+#                if tran + minm > 0:
+#                    transaction[i] = tran + minm
+#                    transaction[minindex] = 0
+#                    temp = {}
+#                    temp["from"] = persons[minindex]
+#                    temp["to"] = persons[i]
+#                    temp["amount"] = - minm
+#                    result.append(temp)
+#                else:
+#                    transaction[i] = 0
+#                    transaction[minindex] = tran + minm
+#                    temp = {}
+#                    temp["from"] = persons[minindex]
+#                    temp["to"] = persons[i]
+#                    temp["amount"] = tran
+#                    result.append(temp)
+#            else:
+#                maxindex = np.argmax(transaction)
+#                maxm = transaction[maxindex]
+#                if tran + maxm > 0:
+#                    transaction[i] = 0
+#                    transaction[maxindex] = tran + maxm
+#                    temp = {}
+#                    temp["from"] = persons[i]
+#                    temp["to"] = persons[maxindex]
+#                    temp["amount"] = - tran
+#                    result.append(temp)
+#                else:
+#                    transaction[maxindex] = 0
+#                    transaction[i] = tran + maxm
+#                    temp = {}
+#                    temp["from"] = persons[i]
+#                    temp["to"] = persons[maxindex]
+#                    temp["amount"] = maxm
+#                    result.append(temp)
+                    
+                    
+    while max(transaction) > 0:
+        maxindex = np.argmax(transaction)
+        minindex = np.argmin(transaction)
+        maxm = transaction[maxindex]
+        minm = transaction[minindex]
+        if maxm + minm > 0:
+            transaction[maxindex] = maxm + minm
+            transaction[minindex] = 0
+            temp = {}
+            temp["from"] = persons[minindex]
+            temp["to"] = persons[maxindex]
+            temp["amount"] = - minm
+            result.append(temp)
+        else:
+            transaction[maxindex] = 0
+            transaction[minindex] = maxm + minm
+            temp = {}
+            temp["from"] = persons[minindex]
+            temp["to"] = persons[maxindex]
+            temp["amount"] = maxm
+            result.append(temp)
     final_result = {}
     final_result["transactions"] = result
     logging.info("My result :{}".format(final_result))
